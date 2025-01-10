52507133ab0ef9f23c894ac329516edcf5527a9c
OpCodeWolf <opcodewolf@proton.me>
2025-01-10 02:01:10 -0800

    Added TODO for viewing logs in the UI

--- src/lib/endpoints/logHandler.ts
+++ src/lib/endpoints/logHandler.ts
@@ -3,6 +3,7 @@ import * as databasae from '../database';
 
 export const logHandler = async (req: Request, res: Response): Promise<any> => {
 
+    // TODO: Add the ability to view the server logs in the Frontend UI via an API request
     console.log(req.body);
 
     res.status(201).json({});
